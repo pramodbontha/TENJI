cd2c184d42d21c8dc42b0428b28c81891b752ef1
pramodbontha <pramodkumar.bontha@gmail.com>
2024-12-04 17:54:48 +0100

    Added helmet package to api and node volumes in compose

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.CLIENT_URL,
   });
+  app.use(helmet());
 
   // Enable global validation with transformation
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
